9c9d91ed4378d94506150977ff543f842e756905
Irina Spasova <irina.spasova.81@gmail.com>
2023-01-15 10:29:04 +0200

    ready

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,16 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-
-
+  // // This block will be executed once the page is loaded and ready
+  // let priceValue = document.querySelector(".price").textContent;
+  // let product = document.querySelector(".product");
+  // product.setAttribute("data-price", priceValue);
+  var productDiv = document.querySelector(".product");
+  var productPrice = document.querySelectorAll("p")[1].innerHTML;
+  var span = document.createElement("span");
+  span.class = "data-price";
+  span.className = "data-price";
+  span.innerText = productPrice;
+  productDiv.setAttribute("data-price", productPrice);
+  productDiv.appendChild(span);
 });
